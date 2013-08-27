730cd25db4f64b3193a162a20e8a8f71436cd35b
Mahemoff <michael@mahemoff.com>
2013-08-27 22:21:42 +0100

    add apppackagename parameter
    For automatic Android installs
    https://developers.google.com/+/web/signin/android-app-installs

--- lib/omniauth/strategies/google_oauth2.rb
+++ lib/omniauth/strategies/google_oauth2.rb
@@ -10,7 +10,7 @@ module OmniAuth
       
       option :skip_friends, true
 
-      option :authorize_options, [:access_type, :hd, :login_hint, :prompt, :scope, :state, :redirect_uri]
+      option :authorize_options, [:access_type, :hd, :login_hint, :prompt, :scope, :state, :redirect_uri, :apppackagename]
 
       option :client_options, {
         :site          => 'https://accounts.google.com',
